0281fc804bb9dc0fbbf0bc5352ea58c580933c98
irfanbaigdev <irfan.blackhawk@gmail.com>
2017-08-17 17:14:05 +0400

    bus transport

---
+++ src/Transport/Bus.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Transport;
+
+/**
+ * Class Bus
+ *
+ * @package \App\Transport
+ */
+class Bus extends BaseTransport
+{
+
+    /**
+     * Return a message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        // TODO: Implement getMessage() method.
+    }
+}
