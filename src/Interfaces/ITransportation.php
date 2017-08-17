8b20f1b68b6c56b92a0e5b70b5307104123d7b1a
irfanbaigdev <irfan.blackhawk@gmail.com>
2017-08-17 17:12:12 +0400

    add interfaces

---
+++ src/Interfaces/ITransportation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Interfaces;
+
+/**
+ * Class ITransportation
+ *
+ * @package \App\Interfaces
+ */
+interface ITransportation
+{
+    /**
+     * Return a message
+     *
+     * @return string
+     */
+    public function getMessage();
+}
